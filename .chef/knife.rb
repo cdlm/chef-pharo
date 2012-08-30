370ee3b6db19a9e527bcc00c28d7248a625e0a13
Damien Pollet <damien.pollet@gmail.com>
2012-08-30 16:10:15 +0200

    Local knife config

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+dotchef = File.dirname(__FILE__)
+
+cookbook_path ["#{dotchef}/../cookbooks"]
+
+cookbook_copyright "Damien Pollet"
+cookbook_email "damien.pollet@inria.fr"
+cookbook_license "mit"
+
